6e280c42aec574b3f7644f607a78d787879b5e8d
sunny <sunny@sunnymix.com>
2023-07-17 23:51:00 +0800

    update gethtml.lua

--- gethtml.lua
+++ gethtml.lua
@@ -3,21 +3,22 @@ ngx.say([[
 <!DOCTYPE html>
 <html>
 <head>
-  <title>读取HTML</title>
+  <title>GET HTML</title>
   <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container-md">
+  <label for="gethtml" class="form-label">URL</label>
   <div class="input-group mb-3">
-    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+    <input type="text" class="form-control" placeholder="URL">
+    <button class="btn btn-primary" type="button" id="gethtml">GET</button>
   </div>
 </div>
 <script>
 $(document).ready(function(){
-  $("h1").click(function(){
+  $("gethtml").click(function(){
     alert("hello");
 });
 });
