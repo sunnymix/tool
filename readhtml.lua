1158f8a7890ba5a597608cb2edbb1a2e60ce28f8
sunny <sunny@sunnymix.com>
2023-07-18 00:45:09 +0800

    update gethtml.lua

---
+++ readhtml.lua
@@ -0,0 +1,18 @@
+local http = require "resty.http"
+
+local httpc = http.new()
+
+local res, err = httpc:request_uri("https://www.gmpsop.com/gmp_documents/qms-020/", {
+    method = "GET"
+})
+
+if not res then
+    ngx.say("请求失败: ", err)
+    return
+end
+
+ngx.say("响应状态码: ", res.status)
+
+ngx.say("响应内容: ", res.body)
+
+httpc:close()
