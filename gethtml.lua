037cc3d5116f9e9fbacc8e3e81d6e2b31a571534
sunny <sunny@sunnymix.com>
2023-07-18 00:23:30 +0800

    update gethtml.lua

--- gethtml.lua
+++ gethtml.lua
@@ -16,10 +16,7 @@ ngx.say([[
     <div class="d-inline-flex p-2"><button type="button" class="btn btn-primary" id="gethtml"> GET HTML </button></div>
   </div>
   <div>
-    <div class="input-group">
-      <span class="input-group-text">HTML</span>
-      <textarea class="form-control"></textarea>
-    </div>
+    <textarea class="form-control"></textarea>
   </div>
 </div>
 <script>
