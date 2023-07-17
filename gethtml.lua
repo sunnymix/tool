a211fdd42dbc0e4c6d2cfb01effe7e4056f1a9e2
sunny <sunny@sunnymix.com>
2023-07-18 00:31:31 +0800

    update gethtml.lua

--- gethtml.lua
+++ gethtml.lua
@@ -13,6 +13,7 @@ ngx.say([[
   <div class="d-flex align-items-center">
     <div class="flex-grow-1 p-2"><input type="text" class="form-control" id="url" placeholder="URL"></div>
     <div class="p-2"><button type="button" class="btn btn-primary" id="gethtml"> GET HTML </button></div>
+    <div class="p-2"><button type="button" class="btn btn-primary" id="copyhtml"> COPY HTML </button></div>
   </div>
   <div class="p-2">
     <textarea class="form-control"></textarea>
