170f2103cb0d29978030f06b99f9bfd192690f5d
sunny <sunny@sunnymix.com>
2023-07-19 01:22:38 +0800

    update lua

--- info.lua
+++ info.lua
@@ -1,15 +1,15 @@
 ngx.header["Content-Type"] = "text/html; charset=utf-8"
 
-ngx.say(package.path)
+ngx.say(package.path .. "<br />")
 
-ngx.say('---')
+ngx.say("---" .. "<br />")
 
-local ok, out = require "resty.shell".run([[echo ok]])
+local ok, out = require "resty.shell".run([[wget -P /tmp/qms-020 https://www.gmpsop.com/gmp_documents/qms-020/]])
 if not ok then
-	ngx.say("cannot exe shell")
+	ngx.say("cannot exe shell" .. "<br />")
 	return
 end
 
-ngx.say(out)
+ngx.say(out .. "<br />")
 
 
