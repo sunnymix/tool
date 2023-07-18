7e7c69e30ef11960f4839ece637b2cec72d5084b
sunny <sunny@sunnymix.com>
2023-07-19 01:36:25 +0800

    update lua

--- readhtml.lua
+++ readhtml.lua
@@ -2,11 +2,9 @@ ngx.header["Content-Type"] = "text/plain; charset=utf-8"
 
 local ok, out = require "resty.shell".run([[wget -O /tmp/qms-020.html https://www.gmpsop.com/gmp_documents/qms-020/]])
 if not ok then
-    ngx.say("wget error" .. "<br />")
+    ngx.say("wget error:" .. "<br />")
     return
 end
-
-ngx.say("wget ok" .. "<br />")
 
 local ok, content = require "resty.shell".run([[cat /tmp/qms-020.html]])
 if not content then
