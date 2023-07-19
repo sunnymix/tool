63a47c57a0438653501d9d250a3ab4acfd0573c8
sunny <sunny@sunnymix.com>
2023-07-19 08:20:35 +0800

    update lua

--- readhtml.lua
+++ readhtml.lua
@@ -1,12 +1,20 @@
-ngx.header["Content-Type"] = "text/plain; charset=utf-8"
+ngx.header["Content-Type"] = "text/html; charset=utf-8"
 
-local ok, out = require "resty.shell".run([[wget -O /tmp/qms-020.html https://www.gmpsop.com/gmp_documents/qms-020/]])
+local args = ngx.req.get_uri_args()
+local url = args["url"]
+
+if not url then
+    ngx.say("url is invalid")
+    return
+end
+
+local ok, out = require "resty.shell".run([[wget -O /tmp/tmp.html ]] .. url)
 if not ok then
     ngx.say("wget error:" .. "<br />")
     return
 end
 
-local ok, content = require "resty.shell".run([[cat /tmp/qms-020.html]])
+local ok, content = require "resty.shell".run([[cat /tmp/tmp.html]])
 if not content then
     ngx.say("cat error:" .. content .. "<br />")
     return
